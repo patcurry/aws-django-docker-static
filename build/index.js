c1a20fef8cc801a34fc4fffa239765cd4705ef7d
currypat <currypat1985@gmail.com>
2018-03-16 10:31:45 +0100

    took build step out and simply forwarding console.log

--- build/index.js
+++ build/index.js
@@ -1,17 +1,1 @@
-(function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
-'use strict';
-
-//import { sum } from './sum';
-
-//const a = Number(document.getElementById('a').innerHTML); // get value of span with innerHTML
-//const b = Number(document.getElementById('b').innerHTML);
-
-//const c = sum(a, b);
-
-//const cElement = document.getElementById('c');
-
-//cElement.append(c);
-
 console.log('yes!');
-
-},{}]},{},[1]);
